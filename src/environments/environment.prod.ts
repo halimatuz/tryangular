1a967b7e49f843024516d2ffd13d26397a68ec3e
halimatuz <h52zzuhriyah@gmail.com>
2021-11-12 13:25:30 +0700

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDtgD3VI1emgXnNhvXW9DDlqiYZxO1cSbA",
+    authDomain: "tryang5-a238e.firebaseapp.com",
+    databaseURL: "https://tryang5-a238e.firebaseio.com",
+    projectId: "tryang5-a238e",
+    storageBucket: "tryang5-a238e.appspot.com",
+    messagingSenderId: "178127675132",
+    appId: "1:178127675132:web:70641b4ee603c5384e8179"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDtgD3VI1emgXnNhvXW9DDlqiYZxO1cSbA",
+    authDomain: "tryang5-a238e.firebaseapp.com",
+    databaseURL: "https://tryang5-a238e.firebaseio.com",
+    projectId: "tryang5-a238e",
+    storageBucket: "tryang5-a238e.appspot.com",
+    messagingSenderId: "178127675132",
+    appId: "1:178127675132:web:70641b4ee603c5384e8179"
+  }
+};
